d5b337d2d54d22bbe45113bf1a4f1ffe6c92b21a
Tsegawit11 <128656324+Tsegawit11@users.noreply.github.com>
2023-10-19 20:56:31 -0400

    Add files via upload

---
+++ validationScript.js
@@ -0,0 +1,34 @@
+    // JavaScript code for form validation
+	// Prevent form from submitting
+
+      // Retrieve the input field value
+       let inputGet = document.querySelector(inputGet)
+       let result = document.querySelector(result)
+       document.getElementById("myForm").addEventListener("click")
+        function fileValidation(){
+        var fileInput= document.getElementById("myForm");
+        const errorElement= document.getElementById("error")
+     
+      
+      };
+
+
+      if(Alphanumeric.valu===''|| Alphanumeric.valu == null) {
+        messages.push(wrrong)
+        
+      }
+        
+      if(messages.Alphanumeric>10){
+        e.preventDefault()
+        errorElement.innerText= message.Alphanumeric
+      }
+       
+    
+        
+      // Regular expression pattern for alphanumeric input
+
+      // Check if the input value matches the pattern
+
+        // Valid input: display confirmation and submit the form
+
+        // Invalid input: display error message
